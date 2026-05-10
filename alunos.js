9f8366324f72bca63e75a382ec7e3ff82b1f6119
afvsf <145919614+afvsf@users.noreply.github.com>
2026-05-10 09:56:00 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const pool = require('./db');
+const auth = require('./middleAuth');
 
 router.get('/', auth, async (req, res) => {
 
